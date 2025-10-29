a991efebf28bfdd4c4667b6e82488ecc73db9fe9
Adolfo Caicaguare <caicaguareadolfo@gmail.com>
2025-10-29 13:32:49 +0100

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/adolcc/blackjack_spring_webflux/BlackjackSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.adolcc.blackjack_spring_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackSpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackSpringWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adolcc/blackjack_spring_webflux/BlackjackSpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adolcc.blackjack_spring_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackSpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
